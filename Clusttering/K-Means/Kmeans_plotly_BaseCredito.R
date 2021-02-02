4d9493ac779cdb21df4225f47fbc389f921842ba
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-02-02 15:38:44 -0300

    #21 Refatoracao do template K-Means

--- Clusttering/K-Means/Kmeans_plotly_BaseCredito.R
+++ Clusttering/K-Means/Kmeans_plotly_BaseCredito.R
@@ -25,18 +25,19 @@ funcaoNA <- function(df){
 
 # Mineracao dos dados
 dados <- read_csv("D:/Projetos_em_R/Machine_Learning/Dados/credit_card_clients.csv")
-dados$ID <- base::factor(dados$ID)
 
 funcaoNA(dados)
 
 dados$BILL_TOTAL <- dados$BILL_AMT1 + dados$BILL_AMT2 + dados$BILL_AMT3 +
   dados$BILL_AMT4 + dados$BILL_AMT5 + dados$BILL_AMT6
+dados$ID <- base::factor(dados$ID)
 dados$SEX <- base::factor(dados$SEX)
 dados$EDUCATION <- base::factor(dados$EDUCATION)
 dados$MARRIAGE <- base::factor(dados$MARRIAGE)
 dados$`default payment next month` <- base::factor(dados$`default payment next month`)
 dplyr::glimpse(dados)
 
+# Base para modelo de 2 atributos
 baseKmeans <- base::data.frame(limite = dados$LIMIT_BAL , gasto = dados$BILL_TOTAL)
 baseKmeans <- base::scale(baseKmeans)
 
@@ -52,7 +53,7 @@ for (i in 1:10) {
 
 graphics::plot(1:10, wcss, type = 'b', xlab = 'Clusters', ylab = 'WCSS')
 
-# Gerando o modelo
+# Gerando o modelo para 2 atributos
 set.seed(1)
 kmeans <- stats::kmeans(x = baseKmeans, centers = 5)
 previsoes <- kmeans$cluster
@@ -63,3 +64,42 @@ glimpse(df)
 graf <- plot_ly(data = df, x=~limite, y=~gasto,
                 color = ~previsoes)
 graf
+
+
+#Base de dados para mais de 2 atributos
+baseKmeans2 <- base::data.frame(limite = dados$LIMIT_BAL,
+                                gasto = dados$BILL_TOTAL,
+                                educacao = dados$EDUCATION,
+                                civil = dados$MARRIAGE,
+                                idade = dados$AGE)
+
+# Normalizacao dos dados
+baseKmeans2$educacao <- base::as.numeric(baseKmeans2$educacao)
+baseKmeans2$civil <- base::as.numeric(baseKmeans2$civil)
+baseKmeans2 <- base::scale(baseKmeans2)
+
+# Elbow method
+base::set.seed(1)
+wcss2 <- base::vector()
+
+for (i in 1:10) {
+  # Gerando o modelo
+  kmeans2 <- stats::kmeans(x = baseKmeans2, centers = i)
+  wcss2[i] <- base::sum(kmeans$withinss)
+}
+
+graphics::plot(1:10, wcss, type = 'b', xlab = 'Clusters', ylab = 'WCSS2')
+
+# Gerando o modelo para 2 atributos
+set.seed(1)
+kmeans2 <- stats::kmeans(x = baseKmeans2, centers = 4)
+previsoes2 <- kmeans2$cluster
+
+# Gerando o grafico interativo
+df2 <- data.frame(baseKmeans2, previsoes2)
+glimpse(df2)
+graf1 <- plot_ly(data = df2, x = ~limite, y = ~gasto,
+                color = ~previsoes2)
+graf1
+
+pairs(baseKmeans2, col = c(1:4)[previsoes])
