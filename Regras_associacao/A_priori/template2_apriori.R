65f9748ff75d0b08b6f91fdcf957aabcc7d4267b
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-02-04 16:27:51 -0300

    25 Refatoracao dos algoritmos a_priori

--- Regras_associacao/A_priori/template2_apriori.R
+++ Regras_associacao/A_priori/template2_apriori.R
@@ -2,10 +2,14 @@ library(readr)
 library(tibble)
 library(arules)
 
+# Mineracao dos dados
 dados <- read.csv2("D:/Projetos_em_R/Machine_Learning/Dados/mercado2.csv",
                    header = FALSE, sep = ",")
 
+# Pre-visualizacao dos dados
 tibble::glimpse(dados)
+
+# Criacao do modelo
 transacoes <- arules::read.transactions("D:/Projetos_em_R/Machine_Learning/Dados/mercado2.csv",
                                         sep = ",", rm.duplicates = TRUE)
 summary(transacoes)
@@ -16,5 +20,6 @@ arules::itemFrequencyPlot(transacoes, topN = 7)
 # Regras
 regras <- arules::apriori(data = transacoes, 
                           parameter = list(support = 0.03, confidence = 0.2))
+
 # Visualizando as regras geradas
 arules::inspect(arules::sort(regras, by = "lift"))
--- Regras_associacao/A_priori/template_apriori.R
+++ Regras_associacao/A_priori/template_apriori.R
@@ -2,10 +2,14 @@ library(readr)
 library(tibble)
 library(arules)
 
+# Mineracao dos dados
 dados <- read.csv2("D:/Projetos_em_R/Machine_Learning/Dados/mercado1.csv",
                    header = FALSE, sep = ",")
 
+# Pre-visualizacao dos dados
 tibble::glimpse(dados)
+
+# Criacao do modelo
 transacoes <- arules::read.transactions("D:/Projetos_em_R/Machine_Learning/Dados/mercado1.csv",
                                    sep = ",", rm.duplicates = TRUE)
 summary(transacoes)
@@ -16,6 +20,7 @@ arules::itemFrequencyPlot(transacoes, topN = 7)
 # Regras
 regras <- arules::apriori(data = transacoes, 
                           parameter = list(support = 0.3, confidence = 0.8))
+
 # Visualizando as regras geradas
 arules::inspect(arules::sort(regras, by = "lift"))
 
